b5e7a9ebe5a1ddefd29f1df53d93289a431fea9d
Jarred Wilson <jarred.wilson12@gmail.com>
2020-07-21 19:46:40 -0400

    Add createTables script

---
+++ scripts/createTables.sql
@@ -0,0 +1,36 @@
+CREATE TABLE authors(
+    AuthorId INT AUTO_INCREMENT,
+    AuthorFirstName VARCHAR(100),
+    AuthorLastName VARCHAR(100),
+    AuthorNationality VARCHAR(50),
+    AuthorDoB DATE,
+    PRIMARY KEY(AuthorId)
+);
+
+CREATE TABLE books(
+    BookID INT AUTO_INCREMENT,
+    BookTitle VARCHAR(100),
+    BookAuthor INT,
+    Genre VARCHAR(50),
+    PRIMARY KEY(BookID),
+    FOREIGN KEY(BookAuthor) REFERENCES authors(AuthorId)
+);
+
+CREATE TABLE clients(
+    ClientId INT AUTO_INCREMENT,
+    ClientFirstName VARCHAR(100),
+    ClientLastName VARCHAR(100),
+    ClientDoB DATE,
+    Occupation VARCHAR(50),
+    PRIMARY KEY(ClientId)
+);
+
+CREATE TABLE borrowers(
+    BorrowId INT AUTO_INCREMENT,
+    ClientId INT,
+    BookId INT,
+    BorrowDate DATE,
+    PRIMARY KEY(BorrowId),
+    FOREIGN KEY(ClientId) REFERENCES clients(ClientId),
+    FOREIGN KEY(BookId) REFERENCES books(BookID)
+);
